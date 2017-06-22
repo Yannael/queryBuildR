44c0e055a11e8f1af2e9c4d554740170d493ce2e
jonathans <yonicd@gmail.com>
2017-06-22 14:18:41 -0400

    add plugin functionality to js

--- R/queryBuildR.R
+++ R/queryBuildR.R
@@ -8,16 +8,14 @@
 #' #### Iris data example.
 #' # Load data
 #' data(iris)
-#' filters<-getFiltersFromTable(iris)
-#' # Set initial rules to NULL
-#' rules<-NULL
-#' # Create Query Builder widget
-#' queryBuildR(rules,filters)
+#' getFiltersFromTable(iris)
 #' @export
 #'
-getFiltersFromTable<-function(data) {
-  filters<-list()
+getFiltersFromTable<-function(data,column_opts=NULL) {
 
+  if(is.null(column_opts)) column_opts=list()
+
+  filters<-list()
   namesCol<-colnames(data)
   niceNames<-as.vector(sapply(namesCol,function(x) gsub('[_.]',' ' ,x)))
 
@@ -25,46 +23,74 @@ getFiltersFromTable<-function(data) {
 
     filterCol<-
       switch(class(data[,i]),
-             character=list(
-                   id= namesCol[i],
-                   label= niceNames[i],
-                   type= 'string',
-                   # input='select',
-                   # plugin='selectize',
-                   # plugin_config=list(valueField="id",
-                   #                    labelField="name",
-                   #                    sortField="name",
-                   #                    searchField="name",
-                   #                    create=FALSE,
-                   #                    options=jsonlite::toJSON(data.frame(id=1:length(unique(data[namesCol[i]])),name=unique(data[,namesCol[i]]),stringsAsFactors = FALSE,row.names = NULL))
-                   # ),
-                   placeholder='Enter search term',
-                   operators=list('equal','not_equal','contains', 'in', 'not_in','begins_with', 'ends_with','is_null', 'is_not_null')),
-             factor={
-               values<-setdiff(levels(data[,i]),"")
-               list(
+             character={
+               char_list=list(
                  id= namesCol[i],
                  label= niceNames[i],
                  type= 'string',
-                 input='select',
-                 values=values,
-                 #default_value=values[1],
                  placeholder='Enter search term',
-                 operators=list('equal','not_equal','contains', 'in', 'not_in','is_null', 'is_not_null'))
+                 operators=switch(as.numeric(is.null(column_opts[[namesCol[i]]][['operators']]))+1,
+                                  column_opts[[namesCol[i]]][['operators']],
+                                  list('equal','not_equal','contains', 'in', 'not_in','begins_with', 'ends_with','is_null', 'is_not_null')
+                                  )
+
+                 )
+
+                    if(column_opts[[namesCol[i]]][['plugin']]=='selectize'){
+                      char_list=c(char_list,list(
+                       input=ifelse(is.null(column_opts[[namesCol[i]]][['input']]),'select',column_opts[[namesCol[i]]][['input']]),
+                       plugin=ifelse(is.null(column_opts[[namesCol[i]]][['plugin']]),'selectize',column_opts[[namesCol[i]]][['plugin']]),
+                       plugin_config=list(
+                                    valueField="id",
+                                    labelField="name",
+                                    sortField="name",
+                                    searchField="name",
+                                    create=TRUE,
+                                    options= jsonlite::toJSON(data.frame(id=1:length(unique(data[,namesCol[i]])),name=unique(data[,namesCol[i]]),stringsAsFactors = FALSE,row.names = NULL))
+                               )))
+                    }
+
+               char_list
+
+
+               },
+
+             factor={
+               list(
+                 id= namesCol[i],
+                 label= niceNames[i],
+                 type= ifelse(is.null(column_opts[[namesCol[i]]][['type']]),'string',column_opts[[namesCol[i]]][['type']]),
+                 input=ifelse(is.null(column_opts[[namesCol[i]]][['input']]),'select',column_opts[[namesCol[i]]][['input']]),
+                 values=setdiff(levels(data[,i]),""),
+                 placeholder='Enter search term',
+                 operators=switch(as.numeric(is.null(column_opts[[namesCol[i]]][['operators']]))+1,
+                                  column_opts[[namesCol[i]]][['operators']],
+                                  list('equal','not_equal','contains', 'in', 'not_in','is_null', 'is_not_null'))
+                 )
              },
+
              integer=list(
                id= namesCol[i],
                label= niceNames[i],
-               type= 'integer',
+               type= ifelse(is.null(column_opts[[namesCol[i]]][['type']]),'integer',column_opts[[namesCol[i]]][['type']]),
+               input=ifelse(is.null(column_opts[[namesCol[i]]][['input']]),'number',column_opts[[namesCol[i]]][['input']]),
                default_value=data[1,i],
-               operators=list('equal','not_equal','less', 'less_or_equal', 'greater','greater_or_equal','between','in', 'not_in','is_null', 'is_not_null')),
+               operators=switch(as.numeric(is.null(column_opts[[namesCol[i]]][['operators']]))+1,
+                                column_opts[[namesCol[i]]][['operators']],
+                                list('equal','not_equal','less', 'less_or_equal', 'greater','greater_or_equal','between','in', 'not_in','is_null', 'is_not_null'))
+               ),
+
              numeric=list(
                id= namesCol[i],
                label= niceNames[i],
                type= 'double',
                default_value=data[1,i],
-               operators=list('equal','not_equal',  'less', 'less_or_equal', 'greater','greater_or_equal','between','is_null', 'is_not_null'))
+               operators=
+                 switch(as.numeric(is.null(column_opts[[namesCol[i]]][['operators']]))+1,
+                        column_opts[[namesCol[i]]][['operators']],
+                        list('equal','not_equal',  'less', 'less_or_equal', 'greater','greater_or_equal','between','is_null', 'is_not_null'))
              )
+      )
     filters<-c(filters,list(filterCol))
   }
   filters
@@ -82,14 +108,19 @@ getFiltersFromTable<-function(data) {
 #' # Load data
 #' data(iris)
 #' filters<-getFiltersFromTable(iris)
-#' # Set initial rules to NULL
-#' rules<-NULL
 #' # Create Query Builder widget
-#' queryBuildR(rules,filters)
+#' queryBuildR(filters)
+#'
+#' queryBuildR(getFiltersFromTable(iris,
+#'                                 column_opts = list(Species=list(type='select',
+#'                                                                 plugin='selectize',
+#'                                 operators=list('equal','not_equal','contains', 'in', 'not_in')))
+#'                                 )
+#'            )
 #'
 #' @export
 #'
-queryBuildR <- function(rules,filters, width = NULL, height = NULL) {
+queryBuildR <- function(filters,rules=NULL,width = NULL, height = NULL) {
 
   x = list(
     rules = rules,
--- inst/htmlwidgets/queryBuildR.js
+++ inst/htmlwidgets/queryBuildR.js
@@ -10,17 +10,37 @@ HTMLWidgets.widget({
 
   renderValue: function(el, x, instance) {
 
+   /*
+   mobileConsole.show();
+
+      mobileConsole.options({
+    		showOnError: false,
+    		proxyConsole: true,
+    		isCollapsed: true,
+    		catchErrors: true
+    	});
+    */
+
     var changeInput = function(id, data) {
         Shiny.onInputChange(el.id + '_' + id, data);
     };
 
     var $el = $(el);
+
+   $el.on('afterCreateRuleInput.queryBuilder', function(e, rule) {
+      if (rule.filter.plugin == 'selectize') {
+        rule.$el.find('.rule-value-container').css('min-width', '200px')
+          .find('.selectize-control').removeClass('form-control');
+      }
+    });
+
     $el.queryBuilder({
       sortable: true,
       filters:x.filters,
       rules: x.rules,
       allow_empty: true
     });
+
 
   },
 
